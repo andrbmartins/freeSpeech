0adad8c07a5ce57760283a7ee77d77156d8400d7
André Martins <andrbmartins@outlook.com>
2017-06-26 11:12:55 +0100

    Created shared package

---
+++ src/org/academiadecodigo/bootcamp8/freespeach/server/utils/UserService.java
@@ -0,0 +1,20 @@
+package org.academiadecodigo.bootcamp8.freespeach.server.utils;
+
+/**
+ * Developed @ <Academia de Código_>
+ * Created by
+ * <Code Cadet> PedroMAlves / Fábio Fernandes
+ */
+public interface UserService {
+
+    boolean authenticate(String username, String pass);
+
+    void addUser(User user);
+
+    void removeUser(String username);
+
+    User getUser(String username);
+
+    int count();
+
+}
