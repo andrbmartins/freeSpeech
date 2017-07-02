a4023d975460e2c769aef1cb623d62f93b633590
Pedro Alves <drpedro.psi@gmail.com>
2017-07-02 23:04:32 +0100

    removing home defs...

--- src/org/academiadecodigo/bootcamp8/freespeech/shared/Values.java
+++ src/org/academiadecodigo/bootcamp8/freespeech/shared/Values.java
@@ -11,7 +11,7 @@ public class Values {
 
     public static final String URL_DBSERVER = "jdbc:mysql://localhost:3306/freespeech";
     public static final String USER_DBSERVER = "root";
-    public static final String PASSWORD_DBSERVER = "babaji";
+    public static final String PASSWORD_DBSERVER = "";
 
     public static final String VIEW_PATH = "../view";
     public static final String CONNECTING_SCENE = "connection";
