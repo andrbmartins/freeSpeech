2854581857660e20b314e44789c87362c1058dac
Pedro <drpedro.psi@gmail.com>
2017-07-05 14:22:27 +0100

    logger verified, db connection being closed on server stop

--- src/org/academiadecodigo/bootcamp8/freespeech/server/utils/logger/LoggerMessages.java
+++ src/org/academiadecodigo/bootcamp8/freespeech/server/utils/logger/LoggerMessages.java
@@ -9,8 +9,9 @@ public class LoggerMessages {
 
     public static final String SERVER_START = "SERVER START";
     public static final String SERVER_STOP = "SERVER STOP";
-    public static final String SERVER_DB_CONNECT = "SERVER CONNECTED DATABASE";
-    public static final String SERVER_DB_DISCONNECT = "SERVER NOT CONNECTED TO DATABASE";
+    public static final String DB_CONNECT = "CONNECTION TO DATABASE ESTABLISHED";
+    public static final String DB_DISCONNECT = "SERVER NOT CONNECTED TO DATABASE";
+    public static final String DB_TERMINATE = "CONNECTION TO DATABASE TERMINATED";
     public static final String CONNECT_CLIENT = "CLIENT CONNECTED";
     public static final String CLIENT_ILLEGAL_LOG = "CLIENT ILLEGAL INIT MESSAGE HEADER";
     public static final String CLIENT_DISCONNECTED = "CLIENT DISCONNECTED: ";
